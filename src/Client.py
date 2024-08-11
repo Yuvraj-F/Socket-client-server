cec00d2cd6a42d61561cca7df46fe6eae9d64e09
Yfa50 <yfa50@uclive.ac.nz>
2024-08-12 00:09:48 +1200

    Removed all comments.

--- src/Client.py
+++ src/Client.py
@@ -13,33 +13,30 @@ Author: Yuvraj Singh Fagotra
 from sys import exit, argv
 from socket import socket, AF_INET, SOCK_DGRAM, getaddrinfo, gaierror
 
-#Globals
 NUM_ARGS = 3
 MIN_PORT = 1024
 MAX_PORT = 64000
-MAGIC_NO = 0x36FB                #Identifies packet as a DateTime packet (16-bits)
-REQUEST_PACKET_TYPE = 0x0001             #Identifies packet as a dt-request packet (16-bits)
+MAGIC_NO = 0x36FB                
+REQUEST_PACKET_TYPE = 0x0001             
 REQUEST_TYPE = {"date": 0x1,
-                "time": 0x2}        #Type of request. 1 = date, 2 = time (16-bits)
-LANG_REP = {1: "English",          #String representaion for each possible language 
-            2: "Māori",            # code in the dt_response packet.
+                "time": 0x2}        
+LANG_REP = {1: "English",          
+            2: "Māori",            
             3: "German"}           
 
-RESPONSE_HEADER_SIZE = 13       #Min length of a dt-response packet, i.e., the lenght of the header 
+RESPONSE_HEADER_SIZE = 13       
 RESPONSE_PACKET_CODE = 2
 MAX_YEAR = 2100
 
 def create_client_socket():
-    """ """
-    sock = None
+    """ Creates socket """
     
+    sock = None
     try:
         sock = socket(AF_INET, SOCK_DGRAM)
     except OSError:
-        
         if sock != None:
-            sock.close()
-            
+            sock.close() 
         print("ERROR: Socket creation failed")
         exit()
         
@@ -50,17 +47,11 @@ def create_client_socket():
 def create_request_packet(request_type):
     """ Creates and returns a dt-request packet. """
     
-    #Initialize packet of size 6 bytes
     packet = bytearray(6)
     
-    #Set MagicNo field
     packet[0] = MAGIC_NO >> 8
     packet[1] = MAGIC_NO & 0xff
-    
-    #Set PacketType field
     packet[3] = REQUEST_PACKET_TYPE
-    
-    #Set RequestType field
     packet[5] = REQUEST_TYPE[request_type]
     
     return packet
@@ -70,28 +61,22 @@ def validate_arguments(args):
     """ makes sure the command line arguments are valid. returns services if 
         given host name or ip address can be resolved """
     
-    #there must be NUM_ARGS arguments
     if len(args) != NUM_ARGS:
         raise ValueError("ERROR: Incorrect number of command line arguments")
-        
-    #First argument must be "date" or "time"
+
     if args[0] not in REQUEST_TYPE.keys():
         raise ValueError(f"ERROR: Request type '{args[0]}' is not valid")
         
-    #Third argument must be positive and in the range [1024, 64000]
     if not args[2].isdigit() or int(args[2]) <= 0:
         raise ValueError(f"ERROR: Given port '{args[2]}' is not a positive integer")
  
     if int(args[2]) < MIN_PORT or int(args[2]) > MAX_PORT:
         raise ValueError(f"ERROR: Given port '{args[2]}' is not in the range [1024, 64000]")
- 
-    #Second argument must be dotted ip address or hostname. If it can be resolved,
-    #extracts the address to use for sending requests to hosts
+
     try:    
         services = getaddrinfo(args[1], args[2], AF_INET, SOCK_DGRAM)
     except gaierror:
         raise ValueError("ERROR: Hostname resolution failed")
-   
         
     return services
 
@@ -142,63 +127,46 @@ def exit_client(socket):
 def main():
     """ Client activities """
     
-    #Get command line arguments
     arguments = argv[1:]
-
-    #Check for error. If valid, get services based on given arguments
+    
     try:    
         services = validate_arguments(arguments)
     except ValueError as err:
         print(err)
         exit()
     
-    #extract Server address from services
     family, sock_type, proto, canonname, server_address = services[0] 
-    
-    #Create socket
     sock = create_client_socket()
-        
-    #Create dt_request packet    
+ 
     request_packet = create_request_packet(arguments[0])
     
-    #Send dt_request packet to server
     try:
         amount = sock.sendto(request_packet, server_address)
     except (OSError, TimeoutError):
         print("ERROR: Sending failed")
         exit_client(sock)
         
-    #Status message if packet is sent to Server successfuly 
     print(f"{arguments[0].capitalize()} request sent to {server_address[0]}:{server_address[1]}")
-    
-    #Extract dt-response packet from socket
+
     try:
         recv_data = sock.recv(64)  
-        
-    #Timeout exceeded
     except TimeoutError:
         print("ERROR: Receiving timed out")
         exit_client(sock)
-    
-    #Receiving failed
     except OSError:
         print("ERROR: Receiving failed")
         exit_client(sock)
-        
 
-    #Check packet validity
     try:
         validate_response(recv_data)
     except ValueError as err:
         print(err)
         exit_client(sock)  
 
-    #Extract data from response packet
     decoded_text = recv_data[13:].decode("utf-8")
     month, day, year = recv_data[8], recv_data[9], recv_data[6] << 8 | recv_data[7]
     hour, minute = recv_data[10], recv_data[11]
     
-    #Print status message that shows the response 
     print(f"{LANG_REP[recv_data[5]]} response received:")
     print(f"Text: {decoded_text}")
     print(f"Date: {day}/{month}/{year}")
--- src/Server.py
+++ src/Server.py
@@ -14,18 +14,17 @@ from select import select
 from datetime import datetime
 
 
-#Globals
 NUM_ARGS = 3
 MIN_PORT = 1024
 MAX_PORT = 64000
 REQUEST_PACKET_SIZE = 6 #Bytes
 REQUEST_PACKET_CODE = 1
 REQUEST_TYPE = {0x1: "date",
-                0x2: "time"}        #Type of request. 1 = date, 2 = time (16-bits)
-MAGIC_NO = 0x36FB               #Identifies packet as a DateTime packet (16-bits)
-RESPONSE_PACKET_TYPE = 0x0002            #Identifies packet as a dt-request packet (16-bits)
-LANG_REP = ["English",          #String representaion for each language 
-            "Māori",            #English, Maori, and German (16-bits)
+                0x2: "time"}        
+MAGIC_NO = 0x36FB               
+RESPONSE_PACKET_TYPE = 0x0002           
+LANG_REP = ["English",          
+            "Māori",            
             "German"]
 
 MONTHS = {1: ("January", "Kohi-tātea", "Januar"),
@@ -94,38 +93,28 @@ def receive_packet(sockets):
         
         Returns None if Error occurs"""
     
-    #Print status message and wait until one of the sockets are ready to be recieved 
     print("Waiting for requests...")
     sock_list, write_sockets, err_sockets = select(sockets, [], [])
-    
-    #Extract socket to send response from
     sock = sock_list[0]
     
-    #Extract dt-request packet and client address 
     try:
         recv_data, client_address = sock.recvfrom(64)  
         
-    #Timeout exceeded
     except TimeoutError:
         print("ERROR: Receiving timed out, dropping packet")
         return
     
-    #Receiving failed
     except OSError:
         print("ERROR: Receiving failed, dropping packet")
         return
     
-    #Check packet validity
     try:
         validate_request(recv_data)
     except ValueError as err:
         print(err)
         return
     
-    #Extract request type from packet
     request_type = recv_data[5]
-    
-    #Status message is packet is valid
     print(f"{LANG_REP[sockets.index(sock)]} received {REQUEST_TYPE[request_type]} request from {client_address[0]}")
     
     return recv_data, client_address, sock
@@ -155,56 +144,29 @@ def create_text_repr(date_time, lang_index, request_type):
 def create_response_packet(data, lang_index, sock):
     """ Creates and returns dt-response packet. """
     
-    #Fetch current date and time
     curr_datetime = datetime.now()
-    
-    #Extract required date and time fields from the datetime object
     day, month, year = curr_datetime.day, curr_datetime.month, curr_datetime.year
     hour, minute = curr_datetime.hour, curr_datetime.minute    
-
-    #Prepare enoded text based on request type
     encoded_text = create_text_repr(curr_datetime, lang_index, data[5]).encode("utf-8")
     
-    #Check to make sure text is at most 255 bytes
     if len(encoded_text) > 255:
         print("ERROR: Text too long, dropping packet")
         return
     
-    #Initialize packet with enough bytes to hold dt-response fixed fields (13-bytes) 
-    # and the encoded text
     packet = bytearray(13 + len(encoded_text))
     
-    #Set MagicNo field
     packet[0] = MAGIC_NO >> 8
     packet[1] = MAGIC_NO & 0xff
-    
-    #Set PacketType field
     packet[3] = RESPONSE_PACKET_TYPE   
-    
-    #Set Language Code field. Computed as lang_index + 1
     packet[5] = lang_index + 1
-    
-    #Set year
     packet[6] = year >> 8
     packet[7] = year & 0xff
-    
-    #Set month
     packet[8] = month
-    
-    #Set day
     packet[9] = day
-    
-    #Set hour
     packet[10] = hour
-    
-    #Set minute
     packet[11] = minute
-    
-    #Set length field which contains the length of the encoded text
     packet[12] = len(encoded_text)
-    
-    #Set text field. i is an index into the packet bytearray initialised to 13
-    # as that is where the text field of a dt-response packet starts
+
     i = 13
     for byte in encoded_text:
         packet[i] = byte
@@ -244,58 +206,40 @@ def main():
     """ args must be three port numbers that are used for English, Maori and German
         respectively. """
     
-    #Get command line arguments
     arguments = argv[1:]
     
-    #Check for errors
     try:
         validate_arguments(arguments)
     except ValueError as err:
         print(err)
         exit()
     
-    #create a seperate socket for each of the three languages. initialise sockets
-    #here so they can be closed later if error occurs
     sockets = [None, None, None]
     try:
         for i in range(len(sockets)):
             sockets[i] = create_server_socket(LANG_REP[i], int(arguments[i]))
-        
-    #If error occurs, close all sockets, print error message, and exit program
     except OSError as err:     
         print(err)
         exit_server(sockets)
-    
-    #Enter receive loop until unhandeld error
+
     try:
         while True:
-            
-            #Retrieve dt-request packet, sending client's address, and the socket
-            # which recieved the request packet
+
             received_data = receive_packet(sockets)
-            
-            #recieve_packet can return None. So check for None case before 
-            # attempting to unpack return values
             if received_data != None:
                 request_packet, client_address, sock = received_data
             else:
                 continue
-            
-            #Create a dt-response packet. Can be None
+
             response_packet = create_response_packet(request_packet, sockets.index(sock), sock)
-            
-            #Send dt_response packet to client if response packet is not None
             if response_packet != None:
                 try:
                     amount = sock.sendto(response_packet, client_address)
                 except (OSError, TimeoutError):
                     print("ERROR: Sending failed, dropping packet")
                     continue
-            
-            #Print status message if response is sent successfully
-            print("Response sent")
-    
-                
+
+            print("Response sent")       
     except Exception as err:
         print(f"ERROR: {err}")
     finally:
